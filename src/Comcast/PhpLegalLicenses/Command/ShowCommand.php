a0f9a3642e0af68533b4afc7b3f86c203766d58e
Matt Armour <marmour@coca-cola.com>
2024-03-27 16:54:36 -0400

    Update return types to match interfaces

--- src/Comcast/PhpLegalLicenses/Command/ShowCommand.php
+++ src/Comcast/PhpLegalLicenses/Command/ShowCommand.php
@@ -25,9 +25,9 @@ class ShowCommand extends DependencyLicenseCommand
      * @param \Symfony\Component\Console\Input\InputInterface   $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
-     * @return void
+     * @return int
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $dependencies = $this->getDependencyList();
         $this->outputDependencyLicenses($dependencies, $output);
